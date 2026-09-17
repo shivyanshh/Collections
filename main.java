5db82736cbedd9fce535f92aa88eecf237c57e4d
Shivyanshh <shivyy2835@gmail.com>
2026-09-17 11:12:23 +0530

    initial

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hii");
+        int[] arr = new int[5]; // fixed size
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(70);
+        System.out.println(list.get(1));
+    }
+}
