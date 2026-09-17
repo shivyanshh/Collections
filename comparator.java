df4331f561776e655f58deb1d7e3ddbcf44f0b84
Shivyanshh <shivyy2835@gmail.com>
2026-09-17 13:47:41 +0530

    arraylist done

---
+++ comparator.java
@@ -0,0 +1,5 @@
+public class comparator {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
--- main.java
+++ main.java
@@ -4,9 +4,46 @@ public class main {
         System.out.println("hii");
         int[] arr = new int[5]; // fixed size
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
+        list.add(90);
         list.add(20);
         list.add(70);
-        System.out.println(list.get(1));
+        list.add(30);
+        list.add(40);
+        list.add(60);
+        list.add(10);
+        list.add(10);
+
+//        System.out.println(list.get(1));
+//        for(int i=0;i<list.size();i++){
+//            System.out.println(list.get(i));
+//        }
+//        for(int x: list){
+//            System.out.println(x);
+//        }list.add(50.2);
+        System.out.println("list size before removing " + list.size());
+        list.remove(2);
+        list.remove(2);
+        list.remove(2);
+        System.out.println("list size after removing "+ list.size());
+        System.out.println(list.size());
+        list.trimToSize();
+        Collections.sort(list);
+        System.out.println(list);
+        System.out.println(list.getClass().getName());
+        //Extra
+        //Array
+        List<String> list1 = Arrays.asList("Monday","Tuesday");
+        System.out.println(list1.getClass().getName());
+        list1.set(1,"Wednesday");
+        System.out.println(list1);
+        //only can swap/replace in index position.
+
+
+        String[] array ={"Apple","Banana","Cherry"};
+        List<String> list2 = Arrays.asList(array);
+        System.out.println(list2.getClass().getName());
+        //only can swap/replace in index position
+
+
     }
 }
